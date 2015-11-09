3260156a6411033de4f57817c3989262f0e41ce6
popovsn777 <popovsn777@gmail.com>
2015-11-09 18:38:41 +0300

    первый запуск Excel с 10 сек выдержкой

--- generator/group.py
+++ generator/group.py
@@ -6,6 +6,42 @@ import string
 import os.path
 import getopt
 import sys
+import time
 
 import clr
-clr.AddReference()
+clr.AddReference('Microsoft.Office.Interop.Excel, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel
+
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    sys.exit(2)
+
+n = 5
+f = "data/groups.xlsx"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="")] + [
+    Group(name=random_string("name", 10))
+    for i in range(n)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+
+excel = Excel.ApplicationClass()
+excel.Visible = True
+
+time.sleep(10)
