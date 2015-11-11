f7d2d7a8d2b07bfb4b5a7ad45aed6c848af76d0b
popovsn777 <popovsn777@gmail.com>
2015-11-11 17:54:46 +0300

    сделал похоже как в уроке (но не пускается)

--- test_somthing.py
+++ test_somthing.py
@@ -4,14 +4,12 @@ import clr
 import os.path
 project_dir = os.path.dirname(os.path.abspath(__file__))
 import sys
-
-
-sys.path.append("TestStack.White.9.2.0.11\\lib\\net40")
-sys.path.append("Castle.Core.3.1.0\\lib\\net40-client")
+sys.path.append(os.path.join(project_dir, "TestStack.White.9.2.0.11\\lib\\net40\\"))
+sys.path.append(os.path.join(project_dir, "Castle.Core.3.1.0\\lib\\net40-client\\"))
 clr.AddReferenceByName('TestStack.White')
 
 from TestStack.White import Application
 
 def test_somthing():
     Application.Launch("notepad.exe")
-    print ('ok')
+    print('ok')
